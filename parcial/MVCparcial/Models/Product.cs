3b5249e015af7f0d7050934096b794be31f892ea
Paulo Cesar Loayza <paulocesarloayza@upsa.edu.bo>
2019-03-30 16:32:46 +0000

    commit con modelo en MVC

---
+++ parcial/MVCparcial/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCparcial.Models
+{
+    public enum UniType
+    {
+        Botella,
+        Metro,
+        Litro,
+        Caja
+    }
+
+    public enum StatusType
+    {
+        Activo,
+        Inactivo
+    }
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+        [Required]
+        public UniType Unit { get; set; }
+
+        public StatusType Status { get; set; }
+    }
+}
